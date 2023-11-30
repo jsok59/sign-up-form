db73cab3decce30c22bddb87ec4419f474e09712
John Sok <98001557+jsok59@users.noreply.github.com>
2023-11-30 18:29:04 -0500

    Finish validating inputs using javascript and styling form

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let password = document.querySelector("#password").value;
-    let confirmPassword = document.querySelector("#confirm_pass").value;
-    let error = document.querySelector(".error")
-    if (password == "" && confirmPassword =="") {
-        error.innerHTML = "*passwords do not match";
-    } else if (password == confirmPassword) {
-        error.innerHTML= ""
-    }
+
+
+
+
+document.querySelector(".submit-btn").addEventListener("click", (event) => {
+
+    checkPassword(event);
+    checkPhone(event);
 });
+
+function checkPassword(event) {
+    let password = document.querySelector('#password').value;
+    let confirmPassword = document.querySelector('#confirm_pass').value;
+    let error = document.querySelector(".error_pass");
+    if (password !== confirmPassword) {
+        error.innerHTML = "* password does not match";
+        
+        event.preventDefault();
+    } else {
+        error.innerHTML = "";
+        
+    }
+}
+
+function checkPhone(event) {
+    let phone_number = document.querySelector("#phone_number").value;
+    let phone_format = /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+    let error = document.querySelector(".error_phone");
+    if (phone_number.match(phone_format)) {
+        console.log(phone_number.match(phone_format))
+        error.innerHTML = "";
+    } else {
+        error.innerHTML = "* phone number must have 10 digits"
+        event.preventDefault();
+    }
+
+}
